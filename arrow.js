5cd5bc73ba67275d9c58a33404c9aa57c91f52fc
mbutu4 <35097218+mbutu4@users.noreply.github.com>
2018-05-24 17:20:43 +0300

    Add files via upload
    arrow function

---
+++ arrow.js
@@ -0,0 +1,14 @@
+function give_back([student]){
+
+	var student = ["Brenda","Nzyuko","Muthoki","mbutu"]
+
+	return student[i]
+
+
+}
+
+result = give_back()
+
+console.log(result)
+
+const answer = (["Brenda","Muthoki","mbutu"]) => {return student[i]};
